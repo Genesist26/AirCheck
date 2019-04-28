faa9b20f2fe6a3512591779b6d67d37ef5fe6080
somsakwp8@outlook.com <23!?github>
2019-04-28 21:29:42 +0700

    init main3-4 and set onclick listener

--- app/src/main/java/com/example/aircheck/Main4Activity.java
+++ app/src/main/java/com/example/aircheck/Main4Activity.java
@@ -2,15 +2,12 @@ package com.example.aircheck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main4Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-
-
+        setContentView(R.layout.activity_main4);
     }
 }
--- app/src/main/java/com/example/aircheck/MainActivity.java
+++ app/src/main/java/com/example/aircheck/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.aircheck;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btPM, btNav, btMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupButton();
+
+    }
+
+    private void setupButton(){
+
+        btPM = findViewById(R.id.bt_pm);
+        btNav = findViewById(R.id.bt_nav);
+        btMap = findViewById(R.id.bt_map);
+
+        btPM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
+
+        btNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main3Activity.class));
+            }
+        });
+
+        btMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main4Activity.class));
+            }
+        });
     }
 }
