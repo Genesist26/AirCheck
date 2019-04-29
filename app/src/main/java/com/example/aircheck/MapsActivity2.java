8a056f23b18be3b467a5cdbd2d99fa89d84a264e
Karn <unihashi.ss@gmail.com>
2019-04-29 20:26:14 +0700

    test

--- app/src/main/java/com/example/aircheck/MapsActivity2.java
+++ app/src/main/java/com/example/aircheck/MapsActivity2.java
@@ -16,7 +16,7 @@ public class MapsActivity2 extends AppCompatActivity implements OnMapReadyCallba
     private GoogleMap mMap;
     final static LatLng default_location = new LatLng(13.75398, 100.50144); //Your LatLong
 
-    int x = 1;
+    int x = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
